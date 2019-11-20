217234b043798125b6fffbfa15f2658399ad8e1f
Ale <alejdelcomp@gmail.com>
2019-11-20 16:00:42 -0600

    fixed folders

--- lex/lex.py
+++ lex/lex.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template
+import os
 
 app = Flask(__name__)
 
+app.static_folder = 'templates'
+environment=os.getenv("ENVIRONMENT","development")
 
 @app.route("/")
 def index():
-    return "Hello from Flask!"
+    return  render_template('index.html')
+
+@app.route("/lex")
+def lex():
+    return "hola lex"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+    debug=False
+    if environment == "development" or environment == "local":
+        debug=True
