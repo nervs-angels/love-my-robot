f728f0534f40a9b55e434ca8b3eab9177a00330c
armi3 <fernandagonzalez@ufm.edu>
2019-11-21 17:28:25 -0600

    rip redis, postie boy ftw

--- gui/server.js
+++ gui/server.js
@@ -1,7 +1,27 @@
 const express = require('express')
-const app = express()
+const redis = require('redis')
 const port = 8080
+const app = express()
+const publisher = redis.createClient({
+    host: 'redis',
+    port: 6379
+})
 
-app.get('/', (req, res) => res.send('Hello From Express'))
+//Set initial visits
+//publisher.set('visits', 0);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+//defining the root endpoint
+app.get('/', (req, res) => {
+    publisher.publish('request-timestamp', 'noviembre 21');
+    res.send("Publishing an Event using Redis");
+    // client.get('visits', (err, visits) => {
+    //     res.send('Number of visits is: ' + visits)
+    //     client.set('visits', parseInt(visits) + 1)
+    // })
+})
+
+//specifying the listening port
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}!`)
+})
--- lex/lex.py
+++ lex/lex.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify, abort
+from redis import Redis
+import json
+import time
 
 app = Flask(__name__)
+# redis = Redis(host='redis', port=6379)
+# subscriber = redis.pubsub(ignore_subscribe_messages=True)
+
 
 
 @app.route("/")
-def index():
-    return "Hello from Flask!"
+def root():
+    # redis.incr('hits')
+    # return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
+
+
+@app.route("/lex", methods = ['POST'])
+def lex():
+    # for message in subscriber.listen():
+    #     rip = message['channel']
+    #     return 'blabla' % rip
+
+    request_body = request.get_json()
+    if not request.json or not 'lmr' in request.json:
+        abort(400)
+
+    return 201
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    # subscriber.subscribe('request-timestamp')
+    app.run(host="0.0.0.0", debug=True)
