e9cdafc1760e11d0b94c5403e0c0492d6fcd605f
Ale <alejdelcomp@gmail.com>
2019-11-21 12:43:24 -0600

    updated lex.py

--- lex/lex.py
+++ lex/lex.py
@@ -17,6 +17,6 @@ def lex():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-    debug=True
+    debug=False
     if environment == "development" or environment == "local":
         debug=True
