d2c305b3e1c1beb662d0fd2eb58c2b09bafec712
Ale <alejdelcomp@gmail.com>
2019-11-21 23:20:54 -0600

    added 'savefile' functions

--- lex/parse.py
+++ lex/parse.py
@@ -1,6 +1,7 @@
 import json
+import os.path, os
 
-def parse():
+def parselmr():
     with open('lex/test.json') as f:
         data = json.load(f)
     
@@ -20,5 +21,22 @@ def parse():
         j=j+1
     
     return array2
+     
+def filename():
+    with open('lex/test.json') as f:
+        data = json.load(f)    
+    request_timestamp=data['request_timestamp']
+    request_timestamp="lmr_lex"+request_timestamp+".py"
+    request_timestamp=request_timestamp.replace("CST","").replace(":","-").replace(" ",'_')
+    request_timestamp=request_timestamp.replace("Sun",'').replace("Mon",'')
+    request_timestamp=request_timestamp.replace('Jan','1').replace("Feb","2").replace("Mar","3").replace("Apr","4").replace("May","5").replace("Jun","6").replace("Jul","7").replace("Aug","8").replace("Sep","9").replace("Oct","10").replace("Nov","11").replace("Dec","12")
+    return (request_timestamp)
+
+def savefile(filename, data):
+    save_path='transpiled/'
+    completeName = os.path.join(save_path, filename) 
+    f = open(completeName,"w")
+    f.write(data)
+    f.close()
+    os.system(completeName)
     
-parse()    
