bfdea680823af32b752e7e01a229967fd43d8155
Ale <alejdelcomp@gmail.com>
2019-11-22 00:28:28 -0600

    updated lmr parsing

--- lex/parse.py
+++ lex/parse.py
@@ -1,6 +1,7 @@
 import json
 import os.path, os
 
+
 def parselmr(json_data):
     
     parsed_json = (json.loads(json_data))
@@ -8,26 +9,23 @@ def parselmr(json_data):
     ##with open('lex/test.json') as f:
         ##data = json.load(f)
     
-    data2= parsed_json['lmr'].split('\n')
-    print(data2)
-    
     array = []
-    for i in data2:
+    for i in parsed_json['lmr']:
         array.append(i)
 
     array2=[]
     j=0     
 
     for i in array:
-        data2[j]=i.split(',')
-        array2.append(data2[j])
+        data[j]=i.split(',')
+        array2.append(data[j])
         j=j+1
     
     return array2
      
 def filename(json_data):
     parsed_json = (json.loads(json_data))
-    ##with open('lex/test.json') as f:
+    ##with open('lemx/test.json') as f:
        ## data = json.load(f)    
     request_timestamp=parsed_json['request_timestamp']
     request_timestamp="lmr_lex"+request_timestamp+".py"
@@ -35,6 +33,8 @@ def filename(json_data):
     request_timestamp=request_timestamp.replace("Sun",'').replace("Mon",'')
     request_timestamp=request_timestamp.replace('Jan','1').replace("Feb","2").replace("Mar","3").replace("Apr","4").replace("May","5").replace("Jun","6").replace("Jul","7").replace("Aug","8").replace("Sep","9").replace("Oct","10").replace("Nov","11").replace("Dec","12")
     return (request_timestamp)
+
+
 
 def savefile(filename, data):
     
@@ -47,3 +47,4 @@ def savefile(filename, data):
 
 
 
+print(parselmr())
